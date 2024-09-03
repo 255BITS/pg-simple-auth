70c3d0c9145d522ca8b4a8e3ec64bd6e70e808f7
Martyn Garcia <martyn.garcia@gmail.com>
2024-09-02 21:44:55 -0600

    add README and pip package

--- pg_simple_auth/__init__.py
+++ pg_simple_auth/__init__.py
@@ -3,6 +3,7 @@ from .auth import initialize, forgot_password, login, signup, verify, decode_tok
 __all__ = [
     'decode_token',
     'forgot_password',
+    'reset_password',
     'initialize',
     'login', 
     'signup',
